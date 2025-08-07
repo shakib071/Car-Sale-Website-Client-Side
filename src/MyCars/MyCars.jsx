736fed23c2d8c6f69cbdfdb1a727ac6a3b38e081
shakib071 <shakibhasan071@gmail.com>
2025-08-08 02:14:46 +0600

    added delete method in my cars and no car layout

--- src/MyCars/MyCars.jsx
+++ src/MyCars/MyCars.jsx
@@ -4,15 +4,16 @@ import Loading from '../Loading/Loading';
 import { useLoaderData } from 'react-router';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import { Link } from 'react-router';
 
 const MyCars = () => {
     const {loading} = use(AuthContext);
     const carDataaaaa = useLoaderData();
-    const [carData,setCarData] = useState(carDataaaaa);
-    
+    const [carData,setCarData] = useState(carDataaaaa);   
     const [openUpdate, setOpenUpdateModal] = useState(false);
     const [availability, setAvailability] = useState('Available');
     const [carToBeUpdated,setCarToBeUpdate] = useState(null);
+
     
     const handleChange = (e) => {
         setAvailability(e.target.value);
@@ -28,7 +29,7 @@ const MyCars = () => {
     // console.log('car to be updated',carToBeUpdated);
 
     const handleUpdateToDatabase=(updatedCarDetails) =>{
-        axios.patch(`http://localhost:5000/update-my-car/${carToBeUpdated._id}`,{"carDetails":updatedCarDetails})
+        axios.patch(`https://car-sale-web-server.vercel.app/update-my-car/${carToBeUpdated._id}`,{"carDetails":updatedCarDetails})
         .then(res => {
             if(res.data.modifiedCount){
                 Swal.fire({
@@ -80,8 +81,43 @@ const MyCars = () => {
         // console.log(updatedCarDetails);
         handleUpdateToDatabase(updatedCarDetails);
     }
+
+    const handleDeleteCar = (car) =>{
+        console.log(car);
+
+        Swal.fire({
+            title: "Are you sure you want to delete it?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+            if (result.isConfirmed) {
+               axios.delete(`http://localhost:5000/delete-cars/${car._id}`)
+                .then(res => {
+                    
+                    if(res.data.deletedCount){
+                        Swal.fire({
+                            position: "center",
+                            icon: "success",
+                            title: "Your Car has been delete",
+                            showConfirmButton: false,
+                            timer: 1500
+                            });
+                        const newDataAfterDelete = carData.filter(item => item._id != car._id);
+                        setCarData(newDataAfterDelete);
+                    }
+                })
+                .then(err => console.log(err))
+            }
+            });
+        
+
+    }
     
-    console.log('car data is' ,carData);
+    
     if(loading){
         return <Loading></Loading>;
     }
@@ -91,7 +127,8 @@ const MyCars = () => {
     return (
         <div>
             <p className='mt-12 text-4xl text-center font-bold'>My Cars</p>
-
+         {
+            carData.length >0 &&
             <div className='mt-10 w-[95%] mx-auto'>
 
                 <div className="overflow-x-auto">
@@ -124,7 +161,7 @@ const MyCars = () => {
                                     <td className='border-2 text-xl font-semibold'>
                                         <div className='flex flex-col gap-2'>
                                             <p onClick={()=>handleModalOpenClose(car)} className='bg-green-500 text-white px-2 py-1 cursor-pointer rounded-md '>Update</p>
-                                            <p className='bg-blue-500 text-white px-2 py-1 cursor-pointer rounded-md '>Delete</p>
+                                            <p onClick={()=> handleDeleteCar(car)} className='bg-red-500 text-white px-2 py-1 cursor-pointer rounded-md '>Delete</p>
                                         </div>
                                     </td>
                                 </tr> 
@@ -236,6 +273,17 @@ const MyCars = () => {
                     </div>
                 )}
             </div>
+         }
+
+         {
+            carData.length ==0 && 
+            <div className='flex flex-col justify-center items-center mt-10'>
+                <p className='text-3xl font-semibold text-red-400'>No Added Cars Available</p>
+                <p className='text-xl mt-3 font-semibold'>You can always add Your car <Link to='/add-car'><span className='btn btn-accent text-white text-xl rounded-2xl shadow-2xl'>Here</span></Link></p>
+            </div>
+         }
+
+
         </div>
     );
 };
