8c9d986380f0a668545dcd1be4ee6b8a2fdfa4f3
shakib071 <shakibhasan071@gmail.com>
2025-08-08 23:40:13 +0600

    added responsive available cars for all  divces

--- src/AvailableCars/AvailableCars.jsx
+++ src/AvailableCars/AvailableCars.jsx
@@ -64,20 +64,20 @@ const AvailableCars = () => {
     return (
         <Suspense fallback={<Loading></Loading>}>
         <div>
-            <p className='mt-10 text-4xl font-bold text-center'>Available Cars</p>
+            <p className='mt-10 text-4xl lg:text-5xl 2xl:text-6xl font-bold text-center'>Available Cars</p>
             
-            <div className='flex justify-center gap-10 items-center w-[92%]  mt-10 mx-auto'>
+            <div className='flex justify-center gap-3  md:gap-10 items-center w-[92%]  mt-10 mx-auto'>
                 <div onClick={handleGridListView} className=''>
                     {
                         isgrid ? (
                             <div className='flex bg-white px-5 py-2 rounded-lg cursor-pointer items-center gap-3'>
-                                <p><FaListUl className='text-2xl text-[#ff6200] shadow-2xl' /></p>
-                                <p className='text-2xl font-semibold'>List View</p>
+                                <p><FaListUl className='text-2xl 2xl:text-4xl  text-[#ff6200] shadow-2xl' /></p>
+                                <p className='text-2xl 2xl:text-4xl font-semibold'>List View</p>
                             </div>
                         ) : (
                             <div className='flex bg-white px-5 py-2 rounded-lg cursor-pointer items-center gap-3'>
-                                <p><IoGrid className='text-2xl  text-[#df0d0d]' /></p>
-                                <p className='text-2xl font-semibold'>Grid View</p>
+                                <p><IoGrid className='text-2xl 2xl:text-4xl  text-[#df0d0d]' /></p>
+                                <p className='text-2xl 2xl:text-4xl font-semibold'>Grid View</p>
                             </div>
                         )
                 
@@ -87,7 +87,7 @@ const AvailableCars = () => {
 
                 <div className='  '>
                     <details className="dropdown ">
-                        <summary className="btn m-1 px-5 py-2 text-2xl font-semibold rounded-lg">Sort Cars</summary>
+                        <summary className="btn m-1 px-5 py-2 2xl:px-10 2xl:py-8 rounded-2xl text-2xl 2xl:text-4xl font-semibold ">Sort Cars</summary>
                         <ul className="menu dropdown-content text-xl bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                             <li onClick={()=> handleSortCar('date-asc')}><a>Sort By Date Asc</a></li>
                             <li onClick={()=> handleSortCar('date-des')}><a>Sort By Date Des</a></li>
@@ -101,33 +101,33 @@ const AvailableCars = () => {
            <div className='mt-7 '>
                 <form onSubmit={handleSearch} className='flex justify-center items-center gap-5' >
                     
-                    <input className='input w-[50%] shadow-md hover:shadow-2xl input-accent' name='search' placeholder='Search a Car by model, brand, or location' type="text" />
-                    <button className='bg-[#52ab32] text-white px-5 py-2 rounded-xl text-xl font-bold' type='submut'>Search</button>
+                    <input className='input w-[60%] md:px-10 md:py-6 lg:py-7 2xl:py-9  text-sm md:text-lg lg:text-xl 2xl:text-2xl shadow-md hover:shadow-2xl input-accent' name='search' placeholder='Search a Car by model, brand, or location' type="text" />
+                    <button className='bg-[#52ab32] text-white px-5 py-2 2xl:px-10 2xl:py-4 rounded-xl text-xl md:text-2xl lg:text-3xl 2xl:text-4xl font-bold' type='submut'>Search</button>
 
                 </form>
            </div>
             {
                 isgrid && (
-                    <div className='mt-10 grid grid-cols-2 gap-7 px-5'>
+                    <div className='mt-10 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-7 px-3 sm:px-7 md:px-15 lg:p-5 '>
                     {
                         allCars.map(car => (
-                            <div key={car._id} className='transition-transform duration-300 transform hover:scale-105  shadow-[0_4px_20px_rgba(36,227,97,0.4)] hover:shadow-[0_4px_30px_rgba(210,173,73,0.8)]  bg-gradient-to-r from-[#61d90bb1] to-[#218bb992] rounded-2xl  p-10'>
+                            <div key={car._id} className='transition-transform duration-300 transform hover:scale-105  shadow-[0_4px_20px_rgba(36,227,97,0.4)] hover:shadow-[0_4px_30px_rgba(210,173,73,0.8)]  bg-gradient-to-r from-[#61d90bb1] to-[#218bb992] rounded-2xl p-5 sm:p-10 md:p-15 lg:px-6 lg:py-12 xl:px-5'>
                                 
                                 <div>
                                     <div>
                                         <img className='w-160 h-80 ' src={car.carDetails.image} alt="car image" />
                                     </div>
-                                    <div className='text-xl flex flex-col gap-1 pl-4'>
+                                    <div className='text-xl md:text-2xl 2xl:text-3xl flex flex-col gap-1 pl-4'>
                                         <p ><span className='font-semibold'>Model: </span>{car.carDetails.carModel}</p>
                                         <p ><span className=' font-semibold'>Rent: </span> ${car.carDetails.dailyRentalPrice}/day</p>
                                         <div className='flex items-center gap-2'>
                                             <p className=' font-semibold'>Availability:</p>
-                                            <p className='text-2xl'> {car.carDetails.available} </p>
+                                            <p className='text-2xl 2xl:text-3xl'> {car.carDetails.available} </p>
                                         </div>
                                         
                                         <p><span className=' font-semibold'>Bookings: </span>{car.carDetails.bookingCount}</p>
                                         <p><span className=' font-semibold'>Date Posted: </span> {car.carDetails.addedDate}</p>
-                                        <Link to={`car-details/${car._id}`}><p className=' text-2xl font-bold btn rounded-xl mt-4'>Book Now</p></Link>
+                                        <Link to={`car-details/${car._id}`}><p className=' text-2xl md:text-3xl 2xl:text-4xl md:px-7 md:py-7 2xl:px-13 2xl:py-9 font-bold btn rounded-xl mt-4'>Book Now</p></Link>
                                     </div>
                                 </div>
                         
@@ -141,29 +141,29 @@ const AvailableCars = () => {
             
             {
                 !isgrid && (
-                <div className='mt-10 grid gap-7 px-5'>
+                <div className='mt-10 grid gap-7 px-5 xl:px-10 2xl:px-12'>
                     {
                         allCars.map(car => (
 
-                            <div key={car._id} className=' transition-transform duration-300 transform hover:scale-103  shadow-[0_4px_20px_rgba(36,227,97,0.4)] hover:shadow-[0_4px_30px_rgba(210,173,73,0.8)]  bg-gradient-to-r from-[#61d90bb1] to-[#218bb992] rounded-2xl  p-8'>
+                            <div key={car._id} className=' transition-transform duration-300 transform hover:scale-103  shadow-[0_4px_20px_rgba(36,227,97,0.4)] hover:shadow-[0_4px_30px_rgba(210,173,73,0.8)]  bg-gradient-to-r from-[#61d90bb1] to-[#218bb992] rounded-2xl px-3 py-4 sm:py-10 md:p-8 lg:p-10 xl:p-12 '>
 
                             
-                                <div className='flex justify-center items-center  gap-20'>
+                                <div className='flex justify-center items-center sm:gap-5 md:gap-20 lg:gap-10'>
                                 <div>
-                                    <img className='w-100 ' src={car.carDetails.image} alt="car image"/>
+                                    <img className='w-50 sm:w-100 md:w-120  ' src={car.carDetails.image} alt="car image"/>
                                 </div>
-                                <div className='text-xl flex flex-col gap-1 pl-4'>
+                                <div className='text-sm sm:text-lg md:text-xl lg:text-2xl 2xl:text-3xl 2xl:py-10 flex flex-col gap-1 pl-4'>
                                     <p ><span className='font-semibold'>Model: </span>{car.carDetails.carModel}</p>
                                     <p ><span className=' font-semibold'>Rent: </span> ${car.carDetails.dailyRentalPrice}/day</p>
                                     
                                     <div className='flex items-center gap-2'>
                                         <p className=' font-semibold'>Availability:</p>
-                                        <p className='text-2xl'> {car.carDetails.available} </p>
+                                        <p className='text-sm md:text-2xl 2xl:text-4xl'> {car.carDetails.available} </p>
                                     </div>
                                     
                                     <p><span className=' font-semibold'>Bookings: </span>{car.carDetails.bookingCount}</p>
                                     <p><span className=' font-semibold'>Date Posted: </span> {car.carDetails.addedDate}</p>
-                                    <Link to='car-details'><p className=' text-2xl font-bold btn rounded-xl mt-4'>Book Now</p></Link>
+                                    <Link to='car-details'><p className=' md:text-2xl 2xl:text-3xl md:px-7 md:py-6 2xl:px-10 2xl:py-7 font-bold btn rounded-xl mt-4'>Book Now</p></Link>
                                 </div>
                                 <div>
                             </div>
