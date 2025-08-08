64b1e4d623b1751271bab4b93a32dcb4bac76ee4
shakib071 <shakibhasan071@gmail.com>
2025-08-09 00:28:11 +0600

    added car details responsive

--- src/CarDetails/CarDetails.jsx
+++ src/CarDetails/CarDetails.jsx
@@ -66,26 +66,26 @@ const CarDetails = () => {
 
     return (
         <div>
-            <p className='mt-10 text-4xl font-bold text-center'>Car Details</p>
+            <p className='mt-10 text-4xl lg:text-6xl font-bold text-center'>Car Details</p>
 
-            <div  className='mt-10 w-[95%] mx-auto bg-gradient-to-r from-[#61d90bb1] to-[#218bb992] rounded-2xl  p-12'>
+            <div  className='mt-10 w-[95%] mx-auto bg-gradient-to-r from-[#61d90bb1] to-[#218bb992] rounded-2xl px-2 py-10 sm:px-10 md:p-12 lg:p-15 2xl:p-17'>
          
                     <div className=''>
                         <div className='flex flex-col items-center'>
                             <img className='w-190 ' src={DetailsData.carDetails.image} alt="car image"/>
                         </div>
-                        <div className='text-xl flex flex-col gap-1 pl-4'>
+                        <div className='text-xl md:text-2xl xl:text-3xl flex flex-col gap-1 pl-4'>
                             <p ><span className='font-semibold'>Model: </span>{DetailsData.carDetails.carModel}</p>
                             <p ><span className=' font-semibold'>Rent: </span> ${DetailsData.carDetails.dailyRentalPrice}/day</p>
                             
                             <div className='flex items-center gap-2'>
                                 <p className=' font-semibold'>Availability:</p>
-                                <p className='text-2xl'> {DetailsData.carDetails.available} </p>
+                                <p className=''> {DetailsData.carDetails.available} </p>
                             </div>
                             
                             <p><span className=' font-semibold'>Features: </span>{DetailsData.carDetails.features}</p>
                             <p><span className=' font-semibold'>Description: </span> <span className='text-[#000000a9]'>{DetailsData.carDetails.description}</span></p>
-                            <Link><p onClick={()=>handleBooking(DetailsData)} className=' text-2xl font-bold btn rounded-xl mt-4'>Book Now</p></Link>
+                            <Link><p onClick={()=>handleBooking(DetailsData)} className='lg:px-10 lg:py-7 text-2xl lg:text-3xl  font-bold btn rounded-xl mt-4'>Book Now</p></Link>
                         </div>
                         <div>
                     </div>
